3246ccaabe714db901ff1d6dfa0eaadde730ab34
kamicloud <cy602939074@qq.com>
2019-09-02 15:28:19 +0800

    init project

---
+++ src/ThemeServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Kamicloud\LaravelStubApiLarecipe;
+
+use BinaryTorch\LaRecipe\LaRecipe;
+use Illuminate\Support\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        LaRecipe::style('laravel-stub-api-larecipe', __DIR__.'/../resources/css/theme.css');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
